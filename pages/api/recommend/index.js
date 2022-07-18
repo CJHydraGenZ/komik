c157fd77df2869d38d520cbc0341a5d1b2df2ac9
Cahya <cahyasaga321@gmail.com>
2022-07-18 21:33:48 +0800

    cache

--- pages/api/recommend/index.js
+++ pages/api/recommend/index.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
     const response = await fetch("https://komikcast.me", {
       method: "GET", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      cache: "force-cache", // *default, no-cache, reload, force-cache, only-if-cached
       credentials: "same-origin", // include, *same-origin, omit
       headers: {
         "Content-Type": "application/json",

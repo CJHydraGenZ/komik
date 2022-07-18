15428f2e13215ffb95bdca4a69be22b6be0dd7da
Cahya <cahyasaga321@gmail.com>
2022-07-18 10:43:26 +0800

    fix domain

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -7,3 +7,5 @@ export default async function handler(req, res) {
 
   res.status(200).json({ name: "John Doe" });
 }
+
+// Access to XMLHttpRequest at 'http://localhost:3000/api/komik' from origin 'https://komik-eight.vercel.app' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.

f23f85cbb91d88f5ccad9d846f54aa16274442a7
Cahya <cahyasaga321@gmail.com>
2022-07-18 17:23:13 +0800

    test

--- pages/chapter/[id].js
+++ pages/chapter/[id].js
@@ -4,8 +4,8 @@ import { server } from "config";
 import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-// import styles from "../styles/Home.module.css";
 
+// import styles from "../styles/Home.module.css";
 export default function Chapter() {
   const router = useRouter();
   const { id } = router.query;

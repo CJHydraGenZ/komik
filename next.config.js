75bac345f220dfd22b4e3e571b7c058427138d19
Cahya <cahyasaga321@gmail.com>
2022-07-18 09:36:24 +0800

    add domain

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    // customKey: "my-value",
-    NEXT_PUBLIC_RAPIDAPI_KEY:
-      "2c852202f9msha06be18b51b4ec6p1ea7d3jsne1c7731ebef2",
-    NEXT_PUBLIC_HOST: "webtoon.p.rapidapi.com",
-  },
+  // env: {
+  //   // customKey: "my-value",
+  //   NEXT_PUBLIC_RAPIDAPI_KEY:
+  //     "2c852202f9msha06be18b51b4ec6p1ea7d3jsne1c7731ebef2",
+  //   NEXT_PUBLIC_HOST: "webtoon.p.rapidapi.com",
+  // },
   images: {
     domains: [
       "img.statically.io",

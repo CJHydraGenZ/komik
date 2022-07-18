d2519942e9e95684be8f4df5f2690022919a1bb3
Cahya <cahyasaga321@gmail.com>
2022-07-18 08:35:18 +0800

    add about fix bun

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,7 +1,37 @@
+import { Button, Timeline } from "flowbite-react";
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Timeline>
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Timeline.Time>Juli 2022</Timeline.Time>
+            <Timeline.Title>Buat UI Komik dan Set API Backend</Timeline.Title>
+            <Timeline.Body>Menggunakan Tailwindcss dan nextjs</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Timeline.Time>Agustus 2022</Timeline.Time>
+            <Timeline.Title>Checking Interface</Timeline.Title>
+            <Timeline.Body>check bug yang masih ada di app</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Timeline.Time>September 2022</Timeline.Time>
+            <Timeline.Title>Publish dan Fix Bug</Timeline.Title>
+            <Timeline.Body>Siapkan domain</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+      </Timeline>
+    </div>
+  );
 };
 
 export default About;

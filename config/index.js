8e244086f45800ed45998709521df6dd33e09b71
Cahya <cahyasaga321@gmail.com>
2022-07-18 13:26:32 +0800

    ad

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "http://localhost:3000";
+export const server = dev
+  ? "http://localhost:3000"
+  : process.env.NEXT_PUBLIC_URL;

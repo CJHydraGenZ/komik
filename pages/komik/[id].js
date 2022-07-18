9e651d913c50751dd2485874b732103a8ab26e5e
Cahya <cahyasaga321@gmail.com>
2022-07-18 12:39:00 +0800

    ADD

--- pages/komik/[id].js
+++ pages/komik/[id].js
@@ -81,7 +81,7 @@ export async function getServerSideProps(context) {
   // console.log("ini endpoint", endpoint);
   const { id } = context.query;
   const res = await fetch(`${server}/api/komik/${id}`, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     credentials: "same-origin", // include, *same-origin, omit
--- pages/komik/index.js
+++ pages/komik/index.js
@@ -45,7 +45,17 @@ export default function Komik({ data }) {
 }
 
 export async function getServerSideProps(context) {
-  const res = await fetch(`${server}/api/komik`);
+  const res = await fetch(`${server}/api/komik`, {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    referrerPolicy: "same-origin",
+  });
   const data = await res.json();
   return {
     props: {

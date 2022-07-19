74b9428707e3a02fb23bf6e6e78ad72e694b9ec1
Cahya <cahyasaga321@gmail.com>
2022-07-19 12:19:53 +0800

    ca

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,7 +3,7 @@
 import { cors, runMiddleware } from "components/middleware";
 
 export default async function handler(req, res) {
-  await runMiddleware(req, res, cors);
+  // await runMiddleware(req, res, cors);
   try {
     //komikidc.vercel
     return res.status(200).json({ name: "John Doe" });

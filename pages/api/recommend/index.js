582c9b6f8d9b7aa781ac824c3828bb4665c2f9f1
Cahya <cahyasaga321@gmail.com>
2022-07-19 10:55:48 +0800

    adad

--- pages/api/recommend/index.js
+++ pages/api/recommend/index.js
@@ -80,7 +80,7 @@ export default async function handler(req, res) {
     return res.status(200).json({
       status: true,
       message: "success",
-      komik_list,
+      data,
     });
   } catch (error) {
     res.status(404).json({ error });

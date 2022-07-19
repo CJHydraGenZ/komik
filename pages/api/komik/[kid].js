04c496b1d4a40b1219da23b6870e51b9deb2ae76
Cahya <cahyasaga321@gmail.com>
2022-07-19 11:50:21 +0800

    fafafa

--- pages/api/komik/[kid].js
+++ pages/api/komik/[kid].js
@@ -1,26 +1,34 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 import { HandlerKomikId } from "components/function/scraping";
-import Cors from "cors";
+import NextCors from "nextjs-cors";
+
+// import Cors from "cors";
 
 // Initializing the cors middleware
-const cors = Cors({
-  methods: ["GET"],
-});
+// const cors = Cors({
+//   methods: ["GET"],
+// });
 
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
+// function runMiddleware(req, res, fn) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result) => {
+//       if (result instanceof Error) {
+//         return reject(result);
+//       }
 
-      return resolve(result);
-    });
-  });
-}
+//       return resolve(result);
+//     });
+//   });
+// }
 
 export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
   const { kid } = req.query;
   // await runMiddleware(req, res, cors);
 

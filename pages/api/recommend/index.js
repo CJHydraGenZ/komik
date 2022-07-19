0546d7483a85debe6467f346589d007ebb021380
Cahya <cahyasaga321@gmail.com>
2022-07-19 11:30:41 +0800

    add

--- pages/api/recommend/index.js
+++ pages/api/recommend/index.js
@@ -28,7 +28,9 @@ export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
   if (req.method === "GET") {
     try {
-      const { data } = await axios.get("https://komikcast.me");
+      const { data } = await axios.get(
+        "https://cors-anywhere.herokuapp.com/https://komikcast.me"
+      );
       // console.log(data);
 
       const $ = cheerio.load(data);

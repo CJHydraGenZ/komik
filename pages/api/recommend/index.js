9a77454d670f6e7c95c9ab9b30c57e99ec11a0db
Cahya <cahyasaga321@gmail.com>
2022-07-19 11:00:15 +0800

    element

--- pages/api/recommend/index.js
+++ pages/api/recommend/index.js
@@ -80,7 +80,7 @@ export default async function handler(req, res) {
     return res.status(200).json({
       status: true,
       message: "success",
-      data,
+      element,
     });
   } catch (error) {
     res.status(404).json({ error });

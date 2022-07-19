ef3d390aa0aff6d08c2581357fc93e6490851d2c
Cahya <cahyasaga321@gmail.com>
2022-07-19 09:43:19 +0800

    cache bermasalah

--- pages/api/recommend/index.js
+++ pages/api/recommend/index.js
@@ -80,9 +80,7 @@ export default async function handler(req, res) {
     return res.status(200).json({
       status: true,
       message: "success",
-      lol: {
-        lol: "sd",
-      },
+      komik_list,
     });
   } catch (error) {
     res.status(404).json({ error });

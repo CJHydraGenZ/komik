85c73054335acc7ec4fbc99870be64dcadc02b6f
Cahya <cahyasaga321@gmail.com>
2022-07-20 18:45:29 +0800

    test recommend

--- components/function/fetch.js
+++ components/function/fetch.js
@@ -28,10 +28,10 @@ export const fetcher = (url) => fetch(url).then((res) => res.json());
 //     .then((res) => res.json())
 //     .then((req) => req.body);
 let headers = new Headers({
-  "Content-Type": "application/json",
+  // "Content-Type": "application/json",
   "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
-  "Accept-Language": "en-GB,en;q=0.5",
+  "Accept-Language": "en-US,en;q=0.9,id;q=0.8",
   Referer: "https://google.com",
   DNT: "1",
   Cahya: "sahhda",

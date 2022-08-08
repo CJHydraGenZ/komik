1dca867bf602973a29093a8ba96aee94c89c6c7c
Cahya <cahyasaga321@gmail.com>
2022-08-08 10:35:24 +0800

    40% dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Dashboard = () => {
+
+  const [open, setOpen] = useState(true)
   return (
     <div className='flex'>
-      <div className={`w-72 h-screen bg-slate-500`}>Sidebar</div>
-      <div className="p-7 text-2xl font-semibold">
+      <div className={`${open ? 'w-72' : 'w-20'} duration-300 h-screen bg-slate-500 relative`}>
+        <button className={`absolute cursor-pointer rounded-full right-3 top-9 border-2 border-slate-400 ${!open && 'rotate-180'}`}
+
+          onClick={() => setOpen(!open)}> c </button>
+      </div>
+      <div className="p-7 text-2xl font-semibold flex-1 h-screen">
         <h1>Home Page</h1>
       </div>
 

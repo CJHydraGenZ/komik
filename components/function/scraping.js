4bf7af07018f580b0527a68872de09976a909606
Cahya <cahyasaga321@gmail.com>
2022-08-15 10:58:19 +0800

    sdada

--- components/function/scraping.js
+++ components/function/scraping.js
@@ -16,121 +16,109 @@ export const HandlerKomikId = async (kid) => {
   //   endpoint = kid;
   // }
 
-  try {
 
 
-    // const API = dev ? `${link_endpoint}/${endpoint}` : `https://api.scrapfly.io/scrape?key=${process.env.SCRAPFLY_API_KEY}&url=https%3A%2F%2Fkomikcast.me%2Fkomik%2F${endpoint}&country=au`
-    // const data = await fetcherAPI(`${link_endpoint}/${endpoint}`);
-    const data = dev ? await fetch(`${link_endpoint}/${endpoint}`).then(res => res.text()) : await fetch(`https://api.scrapfly.io/scrape?key=29f02355ef574070a9a180c6bb2aa420&url=https%3A%2F%2Fkomikcast.me%2Fkomik%2Fthe-hero-returns%2F&tags=player%2Cproject%3Adefault`).then(res => res.json()).then(a => a.result.content);
 
-    console.log(data);
-    // const data = res.body;
-    // console.log("ini data", res.body);
-    const $ = cheerio.load(data);
-    // const element = $(".content");
-    // console.log(element);
-    let genre_list = [];
-    let chapter = [];
-    const obj = {};
-    let info_list = [];
-    /* Get Title, Type, Author, Status */
-    // const getMeta = element.find(".inftable > tbody").first();
-    obj.komik_endpoint = kid;
-    obj.title = $(".komik_info-content-body > h1").text().trim();
-    obj.description = $(".komik_info-content-body > .komik_info-content-native")
-      .text()
-      .trim();
+  // const API = dev ? `${link_endpoint}/${endpoint}` : `https://api.scrapfly.io/scrape?key=${process.env.SCRAPFLY_API_KEY}&url=https%3A%2F%2Fkomikcast.me%2Fkomik%2F${endpoint}&country=au`
+  const data = await fetcherAPI(`${link_endpoint}/${endpoint}`);
+  // const data = dev ? await fetch(`${link_endpoint}/${endpoint}`).then(res => res.text()) : await fetch(`https://api.scrapfly.io/scrape?key=29f02355ef574070a9a180c6bb2aa420&url=https%3A%2F%2Fkomikcast.me%2Fkomik%2Fthe-hero-returns%2F&tags=player%2Cproject%3Adefault`).then(res => res.json()).then(a => a.result.content);
 
-    obj.type = $(".komik_info-content-body > .komik_info-content-meta")
-      .find(".komik_info-content-info-type")
-      .text()
-      .split(":")
-      .splice(1)
-      .join()
-      .trim();
-    obj.author = $(".komik_info-content-body > .komik_info-content-meta")
-      .find("span:nth-child(2)")
-      .text()
-      .split(":")
-      .splice(1)
-      .join()
-      .trim();
-    obj.status = $(".komik_info-content-body > .komik_info-content-meta")
-      .find("span:nth-child(3)")
-      .text()
-      .split(":")
-      .splice(1)
-      .join()
-      .trim();
-    obj.released = $(".komik_info-content-body > .komik_info-content-meta")
-      .find("span:nth-child(1)")
-      .text()
-      .split(":")
-      .splice(1)
-      .join()
-      .trim();
-    obj.total_chapter = $(".komik_info-content-body > .komik_info-content-meta")
-      .find("span:nth-child(5)")
-      .text()
-      .split(":")
-      .splice(1)
-      .join()
-      .trim();
-    obj.update_on = $(".komik_info-content-body > .komik_info-content-meta")
-      .find("span > time")
-      .text()
-      .trim();
-    obj.thumb = $(".komik_info-content > .komik_info-content-thumbnail")
-      .find("img")
-      .attr("src")
-      .replace(/.*?:\/\//g,
-        "https://cdn.statically.io/img/");
+  console.log(data);
+  // const data = res.body;
+  // console.log("ini data", res.body);
+  const $ = cheerio.load(data);
+  // const element = $(".content");
+  // console.log(element);
+  let genre_list = [];
+  let chapter = [];
+  const obj = {};
+  let info_list = [];
+  /* Get Title, Type, Author, Status */
+  // const getMeta = element.find(".inftable > tbody").first();
+  obj.komik_endpoint = kid;
+  obj.title = $(".komik_info-content-body > h1").text().trim();
+  obj.description = $(".komik_info-content-body > .komik_info-content-native")
+    .text()
+    .trim();
 
-    $(".komik_info-content-body > .komik_info-content-genre").each((idx, el) => {
-      let genre_name = $(el)
-        .find("a")
-        .text()
-        .split(/(?=[A-Z])/g);
+  obj.type = $(".komik_info-content-body > .komik_info-content-meta")
+    .find(".komik_info-content-info-type")
+    .text()
+    .split(":")
+    .splice(1)
+    .join()
+    .trim();
+  obj.author = $(".komik_info-content-body > .komik_info-content-meta")
+    .find("span:nth-child(2)")
+    .text()
+    .split(":")
+    .splice(1)
+    .join()
+    .trim();
+  obj.status = $(".komik_info-content-body > .komik_info-content-meta")
+    .find("span:nth-child(3)")
+    .text()
+    .split(":")
+    .splice(1)
+    .join()
+    .trim();
+  obj.released = $(".komik_info-content-body > .komik_info-content-meta")
+    .find("span:nth-child(1)")
+    .text()
+    .split(":")
+    .splice(1)
+    .join()
+    .trim();
+  obj.total_chapter = $(".komik_info-content-body > .komik_info-content-meta")
+    .find("span:nth-child(5)")
+    .text()
+    .split(":")
+    .splice(1)
+    .join()
+    .trim();
+  obj.update_on = $(".komik_info-content-body > .komik_info-content-meta")
+    .find("span > time")
+    .text()
+    .trim();
+  obj.thumb = $(".komik_info-content > .komik_info-content-thumbnail")
+    .find("img")
+    .attr("src")
+    .replace(/.*?:\/\//g,
+      "https://cdn.statically.io/img/");
 
-      genre_list.push(...genre_name);
+  $(".komik_info-content-body > .komik_info-content-genre").each((idx, el) => {
+    let genre_name = $(el)
+      .find("a")
+      .text()
+      .split(/(?=[A-Z])/g);
+
+    genre_list.push(...genre_name);
+  });
+  obj.synopsis = $(".komik_info-description > .komik_info-description-sinopsis")
+    .find("p")
+    .text()
+    .trim();
+
+  obj.genre_list = genre_list || [];
+
+  $(".komik_info-chapters > ul")
+    .find("li")
+    .each((index, el) => {
+      let chapter_title = $(el).find("a").text().trim();
+      let chapter_endpoint = $(el).find("a").attr("href");
+      let chapter_time = $(el).find(".chapter-link-time").text().trim();
+      if (chapter_endpoint !== undefined) {
+        const rep = chapter_endpoint.replace(chapter_link, "");
+        chapter.push({
+          chapter_title,
+          chapter_endpoint: rep,
+          chapter_time,
+        });
+      }
+      obj.chapter = chapter;
     });
-    obj.synopsis = $(".komik_info-description > .komik_info-description-sinopsis")
-      .find("p")
-      .text()
-      .trim();
+  return obj;
 
-    obj.genre_list = genre_list || [];
-
-    $(".komik_info-chapters > ul")
-      .find("li")
-      .each((index, el) => {
-        let chapter_title = $(el).find("a").text().trim();
-        let chapter_endpoint = $(el).find("a").attr("href");
-        let chapter_time = $(el).find(".chapter-link-time").text().trim();
-        if (chapter_endpoint !== undefined) {
-          const rep = chapter_endpoint.replace(chapter_link, "");
-          chapter.push({
-            chapter_title,
-            chapter_endpoint: rep,
-            chapter_time,
-          });
-        }
-        obj.chapter = chapter;
-      });
-    // return obj;
-    return {
-      status: true,
-      message: "success",
-      ...obj,
-    };
-  } catch (error) {
-    return {
-      status: false,
-      message: "gagal",
-      // komik_list,
-      // error
-    };
-  }
 };
 
 export const HandleRecommend = async () => {

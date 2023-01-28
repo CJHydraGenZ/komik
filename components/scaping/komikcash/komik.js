1847b9dd298beefdef0b19cd28d7f606419d5aa8
Cahya <cahyasaga321@gmail.com>
2023-01-28 13:41:09 +0800

    uncomment getListPage

--- components/scaping/komikcash/komik.js
+++ components/scaping/komikcash/komik.js
@@ -3,69 +3,69 @@
 import * as cheerio from "cheerio";
 
 
-// export const getListPage = async (id) => {
-//   try {
-//     const link_endpoint = "https://komikcast.site/komik/";
-//     const chapter_link_endpoint = "https://komikcast.site/chapter/";
-//     const data = await fetch(`https://komikcast.site/daftar-komik/page/${id}`).then(res => res.text())
-//     const $ = cheerio.load(data);
-//     const element = $(".list-update");
-//     // console.log(element.html());
+export const getListPage = async (id) => {
+  try {
+    const link_endpoint = "https://komikcast.site/komik/";
+    const chapter_link_endpoint = "https://komikcast.site/chapter/";
+    const data = await fetch(`https://komikcast.site/daftar-komik/page/${id}`).then(res => res.text())
+    const $ = cheerio.load(data);
+    const element = $(".list-update");
+    // console.log(element.html());
 
-//     let komik_list = [];
-//     let title, type, endpoint, thumb, chapter, rating, last_upload_endpoint;
+    let komik_list = [];
+    let title, type, endpoint, thumb, chapter, rating, last_upload_endpoint;
 
-//     element
-//       .find(".list-update_items-wrapper > .list-update_item")
-//       .each((i, el) => {
-//         title = $(el)
-//           .find("a > .list-update_item-info")
-//           .find("h3")
-//           .text()
-//           .trim();
-//         type = $(el).find("a > .list-update_item-image").find(".type").text();
-//         thumb = $(el)
-//           .find("a > .list-update_item-image")
-//           .find("img")
-//           .attr("src").replace(/.*?:\/\//g,
-//             "https://cdn.statically.io/img/");
-//         // thumb = $(el).find()
-//         chapter = $(el)
-//           .find("a > .list-update_item-info")
-//           .find(".other")
-//           .find(".chapter")
-//           .text()
-//           .trim();
-//         last_upload_endpoint = $(el)
-//           .find("a > .list-update_item-info")
-//           .find(".other")
-//           .find(".chapter")
-//           .attr("href").replace(chapter_link_endpoint, "");
+    element
+      .find(".list-update_items-wrapper > .list-update_item")
+      .each((i, el) => {
+        title = $(el)
+          .find("a > .list-update_item-info")
+          .find("h3")
+          .text()
+          .trim();
+        type = $(el).find("a > .list-update_item-image").find(".type").text();
+        thumb = $(el)
+          .find("a > .list-update_item-image")
+          .find("img")
+          .attr("src").replace(/.*?:\/\//g,
+            "https://cdn.statically.io/img/");
+        // thumb = $(el).find()
+        chapter = $(el)
+          .find("a > .list-update_item-info")
+          .find(".other")
+          .find(".chapter")
+          .text()
+          .trim();
+        last_upload_endpoint = $(el)
+          .find("a > .list-update_item-info")
+          .find(".other")
+          .find(".chapter")
+          .attr("href").replace(chapter_link_endpoint, "");
 
-//         endpoint = $(el).find("a").attr("href").replace(link_endpoint, "");
+        endpoint = $(el).find("a").attr("href").replace(link_endpoint, "");
 
-//         komik_list.push({
-//           title,
-//           type,
-//           thumb,
-//           chapter,
-//           endpoint,
-//           last_upload_endpoint,
-//         });
-//       });
-//     return {
-//       status: true,
-//       message: "success",
-//       komik_list,
-//     };
+        komik_list.push({
+          title,
+          type,
+          thumb,
+          chapter,
+          endpoint,
+          last_upload_endpoint,
+        });
+      });
+    return {
+      status: true,
+      message: "success",
+      komik_list,
+    };
 
-//   } catch (error) {
-//     return {
-//       status: false,
-//       message: error
-//     }
-//   }
-// }
+  } catch (error) {
+    return {
+      status: false,
+      message: error
+    }
+  }
+}
 
 export const getListText = async () => {
   try {

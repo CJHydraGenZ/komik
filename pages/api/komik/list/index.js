aef7f5ef303ec32ef441c0e69043a997dddfd383
Cahya <cahyasaga321@gmail.com>
2023-01-28 13:48:49 +0800

    styling alert

--- pages/api/komik/list/index.js
+++ pages/api/komik/list/index.js
@@ -12,10 +12,7 @@ export default async function handler(req, res) {
     origin: "*",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
-  const { id } = req.query;
-  // await runMiddleware(req, res, cors);
 
-  // console.log("ini", id);
 
   res.status(200).json(await getListText());
 }
--- pages/index.js
+++ pages/index.js
@@ -24,11 +24,11 @@ export default function Home() {
       {/* <div className="container"> */}
       <div className="alert alert-success shadow-lg">
         <div>
-          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-          <span>{data?.author.name}</span>
-          <span>{convertDate(data?.committer.date)}</span>
-          <span>{data?.message}</span>
-          <span>Update</span>
+          <svg xmlns="http://www.w3.org/2000/svg" className="text-white stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+          <span className="text-white">Update</span>
+          <span className="text-white">{data?.author.name}</span>
+          <span className="text-white">{convertDate(data?.committer.date)}</span>
+          <span className="text-white">{data?.message}</span>
           {/* <Link href={data?.html_url}>commit cek</Link> */}
         </div>
       </div>
